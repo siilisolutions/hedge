036f742e4766de598f52f7b88e9fabe65562969e
Janne Kujanpää <janne.kujanpaa@siili.com>
2018-01-19 18:27:40 +0200

    AWS changes
     * code cleanup
     * reflect task naming with azure
     * basic AWS deployment task
       * creates functions.zip from boot output files. Can deploy stack using hardcoded template

--- boot/src/boot_hedge/aws/core.clj
+++ boot/src/boot_hedge/aws/core.clj
@@ -3,18 +3,54 @@
   (:require
    [boot.core          :as c]
    [boot.util          :as u]
-   [boot.task.built-in :refer [sift target]]
+   [boot.task.built-in :refer [sift target zip]]
    [adzerk.boot-cljs :refer [cljs]]
    [clojure.java.io :refer [file]]
-   [boot-hedge.lambda :refer [read-conf generate-files]]
-   [boot-hedge.aws.cloudformation-api :as cf-api]))
+   [boot-hedge.common.core :refer [print-and-return]]
+   [boot-hedge.aws.lambda :refer [read-conf generate-files]]
+   [boot-hedge.aws.cloudformation-api :as cf-api]
+   [boot-hedge.aws.s3-api :as s3-api]))
 
 (c/deftask ^:private function-app
   []
   (c/with-pre-wrap fs
     (-> fs
         read-conf
+        print-and-return
         (generate-files fs))))
+
+(c/deftask ^:private upload-artefact
+  [n stack-name STACK str "Name of the stack"]
+  (c/with-pass-thru [fs]
+    (let [client (s3-api/client)
+          artefact (->> fs
+                        (c/input-files)
+                        (c/by-name #{"functions.zip"})
+                        (first)
+                        (c/tmpfile))]
+      (s3-api/ensure-bucket client stack-name)
+      (s3-api/put-object client stack-name "functions.zip" artefact))))
+
+(c/deftask ^:private deploy-stack
+  [n stack-name STACK str "Name of the stack"]
+  (c/with-pass-thru [fs]
+    (let [client (cf-api/client)
+          cf-file (->> fs
+                       (c/input-files)
+                       (c/by-name #{"cloudformation.yml"})
+                       (first)
+                       (c/tmpfile))]
+      (cf-api/deploy-stack client stack-name cf-file))))
+
+(c/deftask ^:private deploy-to-aws
+  "Deploy fileset to Azure"
+  [n stack-name STACK str "Name of the stack"]
+  (comp
+   (zip :file "functions.zip")
+   ; zip creates artefact we upload!
+   (upload-artefact :stack-name stack-name)
+   ;(create-formation template)
+   (deploy-stack :stack-name stack-name)))
 
 (c/deftask ^:private compile-function-app
   "Build function app(s)"
@@ -25,16 +61,48 @@
    (function-app)
    (cljs :optimizations optimizations)))
 
+(c/deftask create-template
+  []
+  identity)
+
 ; FIXME: 
 ; * if optimizations :none inject :main option (is it even possible)
 ; * read :compiler-options from command line and merge with current config
 ; * rename task later if deployment target for different cloud types is resolved
-(c/deftask deploy-to-target
-  "Build function app(s) and store output to target"
-  [O optimizations LEVEL kw "The optimization level."
-   f function FUNCTION str "Function to compile"]
-  (c/set-env! :function-to-build (or function :all))
+(c/deftask deploy-to-directory
+  "Build lambda(s) and store output to target"
+  [O optimizations LEVEL kw "The optimization level"
+   f function FUNCTION str "Function to compile"
+   d directory DIR str "Directory to deploy into"]
+  (when function (c/set-env! :function-to-build function))
   (comp
     (compile-function-app :optimizations (or optimizations :simple))
+    (create-template)
     (sift :include #{#"\.out" #"\.edn" #"\.cljs"} :invert true)
-    (target)))
+    (target :dir #{(or directory "target")})))
+
+(c/deftask ^:private read-files
+  "Read files from target directory into task fileset"
+  [d directory DIR str "Directory to read from"]
+  (c/with-pre-wrap fs
+    (c/commit! (c/add-resource fs (file (or directory "target"))))))
+
+(c/deftask deploy-from-directory
+  "Deploy files from target directory."
+  [n stack-name STACK str "Name of the stack"
+   d directory DIR str "Directory to deploy from"]
+  (comp
+   (read-files :directory directory)
+   (deploy-to-aws :stack-name stack-name)))
+
+(c/deftask deploy-aws
+  "Build and deploy function app(s)"
+  [n stack-name STACK str "Name of the stack"
+   O optimizations LEVEL kw "The optimization level."]
+  (if (nil? stack-name)
+    (throw (Exception. "Missing stack name"))
+    (comp
+     (compile-function-app :optimizations (or optimizations :advanced))
+     (create-template)
+     (sift :include #{#"\.out" #"\.edn" #"\.cljs"} :invert true)
+     (deploy-to-aws :stack-name stack-name))))
--- boot/src/boot_hedge/aws/lambda.clj
+++ boot/src/boot_hedge/aws/lambda.clj
@@ -6,11 +6,6 @@
    [cheshire.core :refer [generate-stream]]
    [boot.filesystem :as fs]))
 
-
-(defn print-and-return [s]
-  (clojure.pprint/pprint s)
-  s)
-
 (defn read-conf [fileset]
   (->> fileset
        c/input-files
@@ -18,8 +13,7 @@
        first
        c/tmp-file
        slurp
-       clojure.edn/read-string
-       print-and-return))
+       clojure.edn/read-string))
 
 (defn ns-file [ns]
   (-> (name ns)
@@ -92,19 +86,11 @@
 (defn generate-build-files
   "Generates wrapped source codes and edn files for build"
   [fs conf key f]
-  (if (= (c/get-env :function-to-build) :all)
-    (reduce (generate-function f) fs (key conf))
-    (reduce (generate-function f) fs (select-keys (key conf) [(c/get-env :function-to-build)]))))
+  (if (c/get-env :function-to-build)
+    (reduce (generate-function f) fs (select-keys (key conf) [(c/get-env :function-to-build)]))
+    (reduce (generate-function f) fs (key conf))))
 
-(defn generate-files-cf-template
-  "WIP: generate cloudformation template"
-  [fs conf]
-  fs)
-
-; TODO: maybe this should be task?
 (defn generate-files [conf fs]
   "Generates files for build and deploy"
   (-> fs
-    (generate-build-files conf :api generate-source)
-    #_(generate-build-files conf :timer generate-source-timer)
-    (generate-files-cf-template conf)))
+    (generate-build-files conf :api generate-source)))
