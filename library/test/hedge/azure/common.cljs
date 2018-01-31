ec63a0c71acd58a9dfda979292225ea1b5412260
Janne Kujanpää <janne.kujanpaa@siili.com>
2018-01-31 11:48:55 +0200

    Azure timbre: finishing touches. Dummy unit test as documentation

--- library/test/hedge/azure/common.cljs
+++ library/test/hedge/azure/common.cljs
@@ -1,5 +1,6 @@
 (ns hedge.azure.common)
 
+; Not the cleanest solution but works for unit testing
 (def azure-context-logger-mock #(println (str "LOG :: " %)))
 (goog.object/set azure-context-logger-mock "error" #(println (str "LOG :: " %)))
 (goog.object/set azure-context-logger-mock "warn" #(println (str "LOG :: " %)))
