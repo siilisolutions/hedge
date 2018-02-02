e1a143f99f38d21217c9f5284a86779c7cc049ef
Erkki Keränen <erkki.keranen@siili.com>
2018-02-02 08:26:17 +0200

    azure timer initial work

--- boot/src/boot_hedge/azure/function_app.clj
+++ boot/src/boot_hedge/azure/function_app.clj
@@ -4,7 +4,8 @@
    [boot.util          :as util]
    [clojure.string :as str]
    [boot.filesystem :as fs]
-   [boot-hedge.common.core :refer [serialize-json]]))
+   [boot-hedge.core :refer [SUPPORTED_HANDLERS AZURE_FUNCTION]]
+   [boot-hedge.common.core :refer [serialize-json one-handler-config]]))
 
 (defn read-conf [fileset]
   (->> fileset
@@ -42,17 +43,20 @@
     "__"
     (dashed-alphanumeric (name handler))))
 
-(defn generate-source [fs {:keys [handler]}]
-  (let [handler-ns (symbol (namespace handler))
+(defn generate-source [fs {:keys [function type]}]
+  (let [handler (-> function :handler)
+        handler-ns (symbol (namespace handler))
         handler-func (symbol (name handler))
         func-ns (hedge-ns fs handler-ns)
         tgt (c/tmp-dir!)
-        ff (clojure.java.io/file tgt (ns-file func-ns))]
+        ff (clojure.java.io/file tgt (ns-file func-ns))
+        azure-function (get AZURE_FUNCTION type)]
+
     (doto ff
       clojure.java.io/make-parents
-      (spit `(~'ns ~func-ns (:require [hedge.azure.function-app :refer-macros [~'azure-function]]
+      (spit `(~'ns ~func-ns (:require [hedge.azure.function-app :refer-macros [~azure-function]]
                                      [~handler-ns :as ~'handler])))
-      (spit `(~'azure-function ~(symbol (str 'handler "/" handler-func))) :append true))
+      (spit `(~azure-function ~(symbol (str 'handler "/" handler-func))) :append true))
     (clojure.pprint/pprint (slurp ff))
     {:fs (-> fs (c/add-source tgt) c/commit!)
      :func func-ns
@@ -64,7 +68,9 @@
     (spit  {:require [fns]}))
   dir)
 
-(defn function-json [path authorization]
+(defn function-json-for-api 
+  "function.json constructor for function of type api (http in / http out)"
+  [path authorization]
   {:bindings
    [{:authLevel (name authorization),
      :type "httpTrigger",
@@ -74,12 +80,33 @@
     {:type "http", :direction "out", :name "$return"}],
    :disabled false})
 
-(defn generate-function-json [{:keys [fs cloud-name]} path {:keys [authorization] :or {authorization :anonymous}}]
+(defn function-json-for-timer
+  "function.json constructor for function of type timer (timer trigger in). Generates the seconds field as zero."
+  [cron]
+   {:bindings 
+    [{:name "timer",
+      :type "timerTrigger",
+      :direction "in",
+      :schedule (str "0 " cron)}],
+     :disabled false})
+
+(defn generate-function-json 
+  [{:keys [fs cloud-name]} cfg]
   (let [tgt (c/tmp-dir!)
-        func-dir (clojure.java.io/file tgt cloud-name)]
+        func-dir (clojure.java.io/file tgt cloud-name)
+        function-json (cond
+                        (= (-> cfg :type) :api)
+                          (function-json-for-api 
+                            (-> cfg :path) 
+                            (if-let [auth (-> cfg :function :authorization)] auth {:authorization :anonymous})) 
+                        (= (-> cfg :type) :timer)
+                          (function-json-for-timer 
+                            (-> cfg :function :cron))
+                        :else
+                          (throw (Exception. (str "Cannot create function.json for unsupported handler type " type))))]
   (doto (clojure.java.io/file func-dir "function.json")
     clojure.java.io/make-parents
-    (serialize-json (function-json path authorization)))
+    (serialize-json function-json))
   (-> fs (c/add-resource tgt) c/commit!)))
 
 
@@ -90,13 +117,24 @@
         (generate-cljs-edn func))
     (assoc conf :fs (-> fs (c/add-source tgt) c/commit!))))
 
-(defn generate-function [fs [path func]]
+(defn generate-function 
+  "Results in index.js and function.json, receiving a function config as declared in hedge.edn."
+  [fs cfg]
   (->
-    (generate-source fs func)
+    (generate-source fs cfg)
     generate-js-func-compile
-    (generate-function-json path func)))
+    (generate-function-json cfg)))
 
-(defn generate-files [{:keys [api]} fs]
-  (if (c/get-env :function-to-build)
-    (reduce generate-function fs (select-keys api [(c/get-env :function-to-build)]))
-    (reduce generate-function fs api)))
+(defn generate-files 
+  "Entry function for generating the output files. Takes hedge.edn as input. Launches generation of one function if :function-to-build is set."
+  [edn-config fs]
+  (if-let [handler (c/get-env :function-to-build)]
+    ;then
+    (generate-function fs (one-handler-config handler edn-config))
+    ;else
+    (do
+      (let [configs (select-keys edn-config SUPPORTED_HANDLERS)
+            handler-configs (-> (for [config-type (keys configs)]
+                                  (map (fn [item] (one-handler-config (first item) edn-config)) (get configs config-type))) 
+                                flatten)]
+        (reduce generate-function fs handler-configs)))))
--- library/test/hedge/azure/function_app_test.cljs
+++ library/test/hedge/azure/function_app_test.cljs
@@ -3,7 +3,7 @@
             [cljs.core.async :refer [chan put!]]
             [goog.object :as gobj]
             [taoensso.timbre :as timbre]
-            [hedge.azure.function-app :refer [azure-function-wrapper azure->ring] :refer-macros [azure-function]]
+            [hedge.azure.function-app :refer [azure-api-function-wrapper azure->ring] :refer-macros [azure-api-function]]
             [hedge.azure.common :refer [azure-context-logger-mock]]))
 
 (def fixture-once
@@ -49,54 +49,54 @@
   [m handler assertions]
   (testing m
     (async done
-      ((azure-function-wrapper handler)
+      ((azure-api-function-wrapper handler)
          (azure-ctx #(do (assertions %) (done)))
          azure-req))))
             
-(deftest azure-function-wrapper-test-calls-done-after
+(deftest azure-api-function-wrapper-test-calls-done-after
   (testing-azure-wrapper "should call context done with the result given by the handler"
     (constantly "result")
     #(is (= {"body" "result"} %))))
 
-(deftest azure-function-wrapper-test-serialization
+(deftest azure-api-function-wrapper-test-serialization
   (testing-azure-wrapper "should serialize the object returned by handler to camel case js object"
     (constantly {:body {:test-data "Data"}})
     #(is (= (get-in % ["body" "test-data"])  ; TODO: not in camelcase yet
             "Data"))))
 
-(deftest azure-function-wrapper-test-headers
+(deftest azure-api-function-wrapper-test-headers
   (testing-azure-wrapper "should not camel case header fields"
     (constantly {:headers {"Content-Type" "application/json+transit"}})
     #(is (= (get-in % ["headers" "Content-Type"])
             "application/json+transit"))))
             
-(deftest azure-function-wrapper-test-deserialization
+(deftest azure-api-function-wrapper-test-deserialization
   (testing "should deserialize the arguments to maps with dashed keywords"
     (async done
-      ((azure-function-wrapper (fn [req] (is (= {:test-data {:some-field "Data"}} req))))
+      ((azure-api-function-wrapper (fn [req] (is (= {:test-data {:some-field "Data"}} req))))
          (azure-ctx #())
          #js {"testData" #js {"someField" "Data"}}))))
 
-(deftest azure-function-wrapper-test-readport
+(deftest azure-api-function-wrapper-test-readport
     (testing "handler returning a core async ReadPort"
       (async done
         (let [results (chan)
-              azure-fn (azure-function-wrapper (constantly results))]
+              azure-fn (azure-api-function-wrapper (constantly results))]
           (testing "should complete on receival of a message"
             (azure-fn azure-ctx #js {:done #(do (is (= "result" %)) (done))})
             (put! results "result"))))))
 
-(deftest azure-function-test
+(deftest azure-api-function-test
   (testing "azure function"
 
     (testing "should have a cli function that returns nil"
-      (azure-function (constantly :result))
+      (azure-api-function (constantly :result))
 
       (is (= (*main-cli-fn*)
              nil)))
     (testing "should export the a wrapped handler"
       (async done
-        (azure-function (fn [req] (is (= {:test-data {:some-field "Data"}} req))))
+        (azure-api-function (fn [req] (is (= {:test-data {:some-field "Data"}} req))))
         ((gobj/get js/module "exports")
           (azure-ctx #())
           #js {"testData" #js {"someField" "Data"}})))))
